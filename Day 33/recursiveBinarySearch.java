b6abc96e230fd9f0bc4e47b3c553a5f34def1a4c
Saurabh <saurabhsingh4778698@gmail.com>
2022-04-24 23:01:16 +0530

    Day 33

---
+++ Day 33/recursiveBinarySearch.java
@@ -0,0 +1,21 @@
+/**
+ * recursiveBinarySearch
+ */
+public class recursiveBinarySearch {
+
+    public static void main(String[] args) {
+        int arr[]={1,4,5,7,9,13};
+        System.out.println(binarySearch(arr, 0, 5, 1));
+    }
+    static int binarySearch(int[] array,int start,int end,int se){
+        int mid=start+(end-start)/2;
+        if (array[mid]==se) {
+            return mid;
+        } else if (array[mid]<se){
+            return binarySearch(array, mid+1, end, se);
+        }
+        else{
+            return binarySearch(array, start, mid-1, se);
+        }
+    }
+}
